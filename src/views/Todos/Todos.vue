67f0fcc8909eb16280559f87430fec19ae0da1b5
jeancarloscharao <jeancarloscharao@gmail.com>
2022-06-07 07:40:12 -0300

    first commit

---
+++ src/views/Todos/Todos.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="todos">
+    <h1>Lista de Tarefas</h1>
+
+    <ul>
+      <li v-for="todo in todos" :key="todo.identify">
+        {{ todo.name }}
+      </li>
+    </ul>
+
+    <input type="text" v-model="name" />
+  </div>
+</template>
+
+<script>
+import { onMounted } from "vue";
+import TodoService from "@/services/todos.service";
+
+export default {
+  name: "TodosView",
+  setup() {
+    onMounted(() => {
+      TodoService.getAll()
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((error) => console.log(error));
+    });
+  },
+};
+</script>
